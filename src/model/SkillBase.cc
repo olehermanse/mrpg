c1d4249e9860f79c653009406436ac4860af78f9
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-12-16 17:52:33 +0100

    Removed old C/C++ code
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- src/model/SkillBase.cc
+++
@@ -1,7 +0,0 @@
-//  SkillBase.cpp
-//  Created by Ole Herman S. Elgesem on 16/04/14.
-//  Copyright (c) 2014 olehermanse. All rights reserved.
-
-#include "SkillBase.hpp"
-
-SkillBase* SkillBase::sArr[256];
--- src/view/view.hh
+++
@@ -1,5 +0,0 @@
-//  view.h
-//  Created by Ole Herman S. Elgesem on 02/05/14.
-//  Copyright (c) 2014 olehermanse. All rights reserved.
-
-#pragma once
