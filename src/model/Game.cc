109e9ac908e918f2c8fe4861480be1c28b397fa2
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-12-16 17:52:33 +0100

    Removed old C/C++ code
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- src/model/Game.cc
+++
@@ -1,5 +0,0 @@
-//  Game.cpp
-//  Created by Ole Herman S. Elgesem on 26/04/14.
-//  Copyright (c) 2014 olehermanse. All rights reserved.
-
-#include "Game.hpp"
--- src/model/model.hh
+++
@@ -1,7 +0,0 @@
-//  Model.h
-//  Created by Ole Herman S. Elgesem on 26/04/14.
-//  Copyright (c) 2014 olehermanse. All rights reserved.
-#pragma once
-#include "CombatPlayer.hpp"
-#include "Combat.hpp"
-#include "Game.hpp"
