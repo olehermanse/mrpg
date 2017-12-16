01c35add079feb5db9487b6b0128b851ebe06505
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2017-12-16 19:01:07 +0100

    Basic package structure and char creator
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

---
+++ mrpg/__init__.py
@@ -0,0 +1,2 @@
+import sys
+__all__ = ["files"]
---
+++ mrpg/files.py
@@ -0,0 +1,30 @@
+import os
+import json
+import sys
+from collections import OrderedDict
+
+def jsonify(data):
+    return json.dumps(data, indent=2, ensure_ascii=False)
+
+def json_load(data):
+    try:
+        return json.loads(data, object_pairs_hook=OrderedDict)
+    except json.decoder.JSONDecodeError as err:
+        print("ERROR: Invalid json syntax: {}".format(data,err))
+        sys.exit(1)
+
+def save(data, path):
+    folder = os.path.dirname(path)
+    if not os.path.exists(folder):
+        os.makedirs(folder, exist_ok=True)
+    if isinstance(data, dict) or isinstance(data, list):
+        data = jsonify(data)
+    with open(path, 'w', encoding="utf-8") as out_file:
+        out_file.write(data)
+
+def load(path):
+    try:
+        with open(path, 'r', encoding="utf-8") as in_file:
+            return json_load(in_file.read())
+    except FileNotFoundError:
+        return None
