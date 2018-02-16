5bb8d3222ea3cc499f540518d670f071b2ab2900
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-02-16 02:18:47 +0100

    Add tests for creature
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

---
+++ tests/creature_test.py
@@ -0,0 +1,61 @@
+from mrpg.core.creature import Creature
+
+def test_creature():
+    c = Creature("Tester", 2)
+    assert c.name == "Tester"
+    assert c.level == 2
+
+def test_creature_level():
+    c = Creature("Tester", 2)
+    hp = c.base["hp"]
+    msg = c.gain_exp(10000)
+    assert c.level > 2
+    assert c.base["hp"] > hp
+    assert msg
+    for m in msg:
+        print(m)
+        assert type(m) is str
+
+def test_creature_skills():
+    c = Creature("Fighter", 11)
+    skills = ["attack", "heal"]
+    c.set_skills(skills)
+    res = c.get_skill_names()
+    hints = c.get_skill_hints()
+    assert len(res) == len(skills) and len(hints) == len(skills)
+    for a,b in zip(skills,res):
+        assert a.lower() == b.lower()
+
+def test_creature_damage():
+    c = Creature("Fighter", 11)
+    msg = c.damage(c.current["hp"] - 1, limit_check=True)
+    assert msg
+    assert len(msg) > 0
+    assert c.is_alive()
+    msg = c.damage(1, limit_check=True)
+    assert msg
+    assert len(msg) > 0
+    assert c.is_alive() == False
+
+def test_creature_restore():
+    c = Creature("Fighter", 11)
+    msg = []
+    msg += c.damage(c.current["hp"], limit_check=False)
+    msg += c.restore(1, limit_check=True)
+    assert msg
+    assert len(msg) >= 2
+    assert c.is_alive() == True
+    assert c.current["hp"] == 1
+
+def test_creature_export():
+    j = Creature("Jason", 42)
+    data = j.export_data()
+    i = Creature("Tester", 22)
+    i.import_data(data)
+    assert i.name == j.name
+    assert i.level == j.level
+    assert i.base["hp"] == j.base["hp"]
+    for key in j.base:
+        print(key)
+        assert key in i.base
+        assert i.base[key] == j.base[key]
