a5b13c2e810ace73fe87df35a41857fdcf5e1981
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-04-10 00:33:33 +0200

    GUI: Backgreound and title text
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- mrpg/gui/renderer.py
+++ mrpg/gui/renderer.py
@@ -3,6 +3,15 @@
 import pyglet
 
 
+class Color:
+    # Off black and white look a little more pleasant
+    BLACK = (16, 16, 16, 255)
+    WHITE = (230, 230, 230, 255)
+
+    def float(c):
+        return (x / 255 for x in c)
+
+
 class Renderer():
     def __init__(self, controller, window):
         self.controller = controller
@@ -10,6 +19,20 @@ class Renderer():
         cursor = window.get_system_mouse_cursor("crosshair")
         window.set_mouse_cursor(cursor)
 
+        self.labels = []
+        title = pyglet.text.Label(
+            'MRPG - Prototype',
+            font_name='Arial',
+            font_size=36,
+            x=0,
+            y=self.window.height,
+            anchor_x='left',
+            anchor_y='top',
+            color=Color.WHITE)
+        self.labels.append(title)
+
     def render(self):
         self.window.clear()
-        pyglet.gl.glClearColor(255, 255, 255, 255)
+        pyglet.gl.glClearColor(*Color.float(Color.BLACK))
+        for label in self.labels:
+            label.draw()
