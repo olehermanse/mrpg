11a7ac29946f72ad46e4142c04f0013bc708b5b1
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-04-17 03:13:18 +0200

    Restructured skills to be more clear and flexible
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- mrpg/core/skills.py
+++ mrpg/core/skills.py
@@ -1,68 +1,109 @@
-from collections import OrderedDict
+from mrpg.utils.utils import printable, internal
 
-from mrpg.core.skill import Skill
+class SkillUse:
+    def __init__(self, name=None, hint=None):
+        self.name = name
+        self.hint = hint
+        self.preparer = None
+        self.resolver = None
+        self.user = None
+        self.target = None
+
+    def func_pair(self, preparer, resolver):
+        self.preparer, self.resolver = preparer, resolver
+
+    def prepare(self, user, target):
+        skill = self
+        self.user = user
+        self.target = target
+        res = self.preparer(skill, user, target)
+        if not res:
+            return []
+        return res
+
+    def resolve(self):
+        return self.resolver(self, self.user, self.target)
 
 
 class SkillFuncs:
-    def attack(user, target, hint=False):
-        if hint:
-            return "Physical attack"
-        usr, tar = user.current, target.current
-        damage = 2 * usr["str"]
-        damage -= tar["str"]
-        damage = max([damage, 1])
+    def attack():
+        obj = SkillUse(hint="Physical attack")
 
-        def resolve():
-            return target.damage(damage)
+        def prepare(skill, user, target):
+            usr, tar = user.current, target.current
+            damage = 2 * usr["str"]
+            damage -= tar["str"]
+            damage = max([damage, 1])
+            skill.power = damage
 
-        return resolve
+        def resolve(skill, user, target):
+            return target.damage(skill.power)
 
-    def fireball(user, target, hint=False):
-        if hint:
-            return "Hot magic"
-        usr, tar = user.current, target.current
-        damage = 2 * usr["int"]
-        damage -= tar["int"]
-        damage = max([damage, 1])
+        obj.func_pair(prepare, resolve)
 
-        def resolve():
-            return target.damage(damage)
+        return obj
 
-        return resolve
+    def fireball():
+        obj = SkillUse(hint="Hot magic")
 
-    def life_drain(user, target, hint=False):
-        if hint:
-            return "Damage and restore"
-        usr, tar = user.current, target.current
-        amount = 3 * usr["int"] // 2
-        amount -= tar["int"]
-        amount = max([amount, 2])
-        amount = min([amount, user.base["hp"]])
+        def prepare(skill, user, target):
+            usr, tar = user.current, target.current
+            damage = 2 * usr["int"]
+            damage -= tar["int"]
+            damage = max([damage, 1])
+            skill.power = damage
 
-        def resolve():
-            return target.damage(amount) + user.restore(amount)
+        def resolve(skill, user, target):
+            return target.damage(skill.power)
 
-        return resolve
+        obj.func_pair(prepare, resolve)
 
-    def heal(user, target, hint=False):
-        if hint:
-            return "Heal self"
-        amount = 2 * user.current["int"]
-        amount = min([amount, user.base["hp"]])
+        return obj
 
-        def resolve():
-            return user.restore(amount)
+    def life_drain():
+        obj = SkillUse(hint="Damage and restore")
 
-        return resolve
+        def prepare(skill, user, target):
+            usr, tar = user.current, target.current
+            amount = 3 * usr["int"] // 2
+            amount -= tar["int"]
+            amount = max([amount, 2])
+            amount = min([amount, user.base["hp"]])
+            skill.power = amount
 
+        def resolve(skill, user, target):
+            return target.damage(skill.power) + user.restore(skill.power)
+
+        obj.func_pair(prepare, resolve)
+
+        return obj
+
+    def heal():
+        obj = SkillUse(hint="Heal self")
+
+        def prepare(skill, user, target):
+            amount = 2 * user.current["int"]
+            amount = min([amount, user.base["hp"]])
+            skill.power = amount
+
+        def resolve(skill, user, target):
+            return skill.user.restore(skill.power)
+
+        obj.func_pair(prepare, resolve)
+
+        return obj
 
 class Skills:
     def get(name):
         if type(name) is not str:
             name = name.name
-        name = name.lower()
-        name = name.replace(" ", "_")
-        skill_func = getattr(SkillFuncs, name)
-        return Skill(name, skill_func)
+        internal_name = internal(name)
+        default_name = printable(internal_name)
+        skill_func = getattr(SkillFuncs, internal_name)
+        skill_obj = skill_func()
+        if not skill_obj.name:
+            assert "'" not in name  # Skills with quotes must always set name
+            skill_obj.name = default_name
+        return skill_obj
 
     names = [x for x in dir(SkillFuncs) if "__" not in x]
