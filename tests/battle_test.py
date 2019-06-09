4dc273b7076f0f1cd58e66f16afcc45e59a45a5c
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-06-09 01:23:00 +0100

    Added tests for speed and effects
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- tests/battle_test.py
+++ tests/battle_test.py
@@ -1,6 +1,23 @@
 from mrpg.core.battle import Battle, Turn
 from mrpg.core.game import new_player
 from mrpg.core.event import Event
+from mrpg.core.effects import Effects
+
+
+def one_turn(battle):
+    assert battle.turn is None
+    battle.turn = Turn(battle)
+    printed_something = False
+    for event in battle.turn.events:
+        assert type(event) is Event
+        messages = event.apply()
+        assert type(messages) is list
+        for msg in messages:
+            assert type(msg) is str
+            assert len(msg.strip()) > 0
+            printed_something = True
+    assert printed_something
+    battle.turn = None
 
 
 def test_battle():
@@ -11,16 +28,80 @@ def test_battle():
         while not battle.is_over():
             battle.a.ai()
             battle.b.ai()
-            battle.turn = Turn(battle)
-            printed_something = False
-            for event in battle.turn.events:
-                assert type(event) is Event
-                messages = event.apply()
-                assert type(messages) is list
-                # assert len(messages) > 0
-                for msg in messages:
-                    assert type(msg) is str
-                    assert len(msg.strip()) > 0
-                    printed_something = True
-            assert printed_something
-            battle.turn = None
+            one_turn(battle)
+        assert a.is_dead() or b.is_dead()
+
+
+def test_speed():
+    """Test that a player with higher speed will act first"""
+    player = new_player()
+    skills = player.skills.equipped.names()
+    del player
+
+    assert len(skills) > 3
+    assert "Attack" in skills
+
+    for defender_skill in skills:
+        a, b = new_player(), new_player()
+        a.set_level(b.level + 1)
+        battle = Battle(a, b)
+        a.pick_skill("Attack")
+        b.pick_skill(defender_skill)
+        a.current["hp"] = b.current["hp"] = 1
+        one_turn(battle)
+        assert a.is_alive() and not b.is_alive()
+        assert not a.is_dead() and b.is_dead()
+
+
+def test_burn():
+    """Test that burn can damage and kill, and you can't heal with 0 mana"""
+    a, b = new_player(), new_player()
+    battle = Battle(a, b)
+
+    a.pick_skill("Heal")
+    b.pick_skill("Heal")
+    a.current["hp"] = b.current["hp"] = 2
+    a.current["mp"] = b.current["mp"] = 0
+
+    burn = Effects.get("burn", target=a)
+    burn.message = "{} was burned".format(a.name)
+    burn.damage = 1
+    burn.duration = 1
+    a.add_effect(burn)
+
+    burn = Effects.get("burn", target=b)
+    burn.message = "{} was burned".format(b.name)
+    burn.damage = 1
+    burn.duration = 2
+    b.add_effect(burn)
+
+    assert a.current["hp"] == b.current["hp"] == 2
+    assert a.current == b.current
+
+    one_turn(battle)
+    assert a.current["hp"] == b.current["hp"] == 1
+    assert a.is_alive() and b.is_alive()
+
+    one_turn(battle)
+    assert a.is_alive() and b.is_dead()
+
+
+def test_shock():
+    """Test that shock correctly reduces dexterity"""
+    a, b = new_player(), new_player()
+    battle = Battle(a, b)
+    a.pick_skill("Heal")
+    b.pick_skill("Heal")
+    a.current["hp"] = b.current["hp"] = 1
+    a.current["mp"] = b.current["mp"] = 0
+    a.add_effect(Effects.get("shock", target=a))
+
+    # Effects don't do anything before their events are applied:
+    assert a.current["dex"] == b.current["dex"]
+    assert a.current == b.current
+
+    one_turn(battle)
+
+    assert a.is_alive() and b.is_alive()
+    assert a.current["dex"] < b.current["dex"]
+    assert a.current != b.current
