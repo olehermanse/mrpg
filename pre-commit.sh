ec3259b70bdd2594e15a5009936ff3e084209d38
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2019-06-09 01:41:54 +0100

    Added pre-commit hook to run yapf and tests
    Inspired by:
    https://github.com/cfengine/masterfiles/pull/1418
    
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

---
+++ pre-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Run format and test before commiting
+#   cp -a pre-commit.sh .git/hooks/pre-commit
+
+fail=0
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+yapf --recursive . --diff   || fail="${RED}Commit hook failed: Formatting - run make yapf${NC}"
+test "$fail" = 0 || echo $fail
+test "$fail" = 0 || exit 1
+
+py.test                     || fail="${RED}Commit hook failed: Formatting - run make check${NC}"
+test "$fail" = 0 || echo $fail
+test "$fail" = 0 || exit 1
